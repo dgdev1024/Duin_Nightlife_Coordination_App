50410f85de1cc54d914185c0f6ddd91f84b5efdc
Dennis Griffin <dgdev1024@gmail.com>
2017-09-26 10:33:57 -0400

    [170926] Added postinstall command to package.json...

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ mongoose.Promise = global.Promise;
 //
 // Comment this line out when you are ready to deploy this
 // application!
-// loadenv('.env');
+loadenv('.env');
 
 // Connect to Database
 mongoose.connect(process.env.DATABASE_URL, { useMongoClient: true })
@@ -22,5 +22,6 @@ mongoose.connect(process.env.DATABASE_URL, { useMongoClient: true })
     .catch(err => {
         // Report the error and exit the program.
         console.error(`[EXCEPTION!] ${err}`);
+        console.error(err.stack || 'No Stack');
         mongoose.connection.close().catch(() => process.exit(1));
     });
