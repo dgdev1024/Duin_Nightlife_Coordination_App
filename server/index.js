19698cb507838fb6c1114db6ad7658e8080421e7
Dennis Griffin <dgdev1024@gmail.com>
2017-09-26 10:13:17 -0400

    Preparing to deploy to Heroku...

--- server/index.js
+++ server/index.js
@@ -32,6 +32,15 @@ module.exports = () => {
     app.use(bodyParser.urlencoded({ extended: false }));
     app.use(express.static(path.join(__dirname, '..', 'dist')));
 
+    // Some custom middleware to force HTTPS if we are on Heroku.
+    // SOURCE: https://stackoverflow.com/a/31144924/2868302
+    app.use((req, res, next) => {
+        if (!req.secure && req.get('x-forwarded-proto') !== 'https' && process.env.NODE_ENV !== "development") {
+            return res.redirect('https://' + req.get('host') + req.url);
+        }
+        next();
+    });
+
     // Socket.IO
     const server = http.createServer(app);
     const socket = socketIo(server);
